2ae63ffb0e6e756c8800cfb69dee0544e426dd02
Dmitry Kozyrev <trumphack@gmail.com>
2023-01-16 17:30:44 -0600

    add urls.py

---
+++ yatube/posts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# ice_cream/views.py
+from django.http import HttpResponse
+
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница со списком мороженого
+def group_posts(request, slug):
+    return HttpResponse(f'Список мороженого {slug}')
+
